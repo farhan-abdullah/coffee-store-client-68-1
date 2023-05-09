f9f51249e1d2627004bfa00f1499e904bca31d29
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-09 22:05:06 +0200

    68-8

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
-	const { name, quantity, supplier, taste, details, photo, category } = coffee;
+	const { _id, name, quantity, supplier, taste, details, photo, category } = coffee;
+
+	const handleDelete = (_id) => {
+		console.log(_id);
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				console.log('delete conferm');
+				fetch(`http://localhost:5000/coffee/${_id}`, {
+					method: 'DELETE',
+				})
+					.then((res) => res.json())
+					.then((data) => {
+						console.log(data);
+						if (data.deletedCount > 0) {
+							Swal.fire('Deleted!', 'Your coffee item has been deleted.', 'success');
+						}
+					});
+			}
+		});
+	};
+
 	return (
-		<div className='card   card-side bg-base-100 shadow-xl'>
+		<div className='card card-side bg-base-100 shadow-xl p-2'>
 			<figure>
 				<img src={photo} alt='Movie' />
 			</figure>
@@ -17,8 +47,14 @@ const CoffeeCard = ({ coffee }) => {
 				<div className='card-actions justify-end '>
 					<div className='flex flex-col gap-5'>
 						<button className='btn'>View</button>
-						<button className='btn'>Update</button>
-						<button className='btn bg-orange-600 border-none'>X</button>
+						<Link to={`/updatecoffee/${_id}`}>
+							<button className='btn'>Update</button>
+						</Link>
+						<button
+							onClick={() => handleDelete(_id)}
+							className='btn bg-orange-600 border-none'>
+							X
+						</button>
 					</div>
 				</div>
 			</div>
--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UpdateCoffee = () => {
-	return <div></div>;
+	const data = useLoaderData();
+	return (
+		<div>
+			<h1>{data.name}</h1>
+		</div>
+	);
 };
 
 export default UpdateCoffee;
